35081ea2c1e3209d64cd805d0ac12bc2eb44d8ce
DALM1 <92697916+DALM1@users.noreply.github.com>
2022-10-13 15:42:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ window.onload = function () {
   w = canvas.width = window.innerWidth;
   h = canvas.height = window.innerHeight;
 
-  //初始化
+  
   var clearColor = "rgba(0, 0, 0, .1)", //用于绘制渐变阴影
     wordColor = "#00ced1", //文字颜色
     words =
@@ -24,12 +24,12 @@ window.onload = function () {
     context.save();
     context.fillStyle = wordColor;
     context.font = font_size + "px arial";
-    //核心
-    // 每次请求动画只绘制一行, 所以fillRect会产生渐隐的效果
+    
+    
     for (var i = 0; i < drops.length; i++) {
       var text = wordsArr[Math.floor(Math.random() * wordsArr.length)];
       context.fillText(text, i * font_size, drops[i] * font_size);
-      // 随机回头重绘
+      
       if (drops[i] * font_size > h && Math.random() > 0.98) {
         drops[i] = 0;
       }
@@ -38,7 +38,7 @@ window.onload = function () {
     context.restore();
   }
 
-  //动画循环
+  
   (function drawFrame() {
     window.requestAnimationFrame(drawFrame);
     context.fillStyle = clearColor;
